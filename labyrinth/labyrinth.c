fffa69246c000c065e9d1e87a1c9a2412ac1e947
Brighton <1205704350@qq.com>
2026-02-04 11:24:16 +0800

    feat: implement isVaildPlayer and loadMap

--- labyrinth/labyrinth.c
+++ labyrinth/labyrinth.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <assert.h>
 #include <testkit.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "labyrinth.h"
 
 int main(int argc, char *argv[]) {
     // TODO: Implement this function
+    Labyrinth labyrinth;
+    labyrinth.cols = labyrinth.rows = 0;
+    loadMap(&labyrinth, "./maps/map.txt");
     return 0;
 }
 
@@ -20,15 +24,51 @@ void printUsage() {
 }
 
 bool isValidPlayer(char playerId) {
-    // TODO: Implement this function
+    if(playerId >= '0' && playerId <= '9')
+        return true;
     return false;
 }
-
+UnitTest(testIsVaildPlayer) {
+    assert(isValidPlayer('5') == true);
+    assert(isValidPlayer('a') == false);
+}
 bool loadMap(Labyrinth *labyrinth, const char *filename) {
-    // TODO: Implement this function
-    return false;
+    int fd = open(filename, O_RDONLY);
+    if(fd < 0) {
+        perror("Open file failed.");
+        return false;
+    }
+    // printf("%d %d\n", labyrinth->rows, labyrinth->cols);
+    // printf("%d\n", fd);
+    char buf[MAX_COLS * MAX_ROWS];
+    ssize_t n;
+    while((n = read(fd, buf, sizeof(buf))) > 0) {
+        for(int i = 0; i < n; ++i) {
+            labyrinth->map[labyrinth->rows][labyrinth->cols++] = buf[i];
+            if(buf[i] == '\n') {
+                labyrinth->map[labyrinth->rows][labyrinth->cols] = '\0';
+                ++labyrinth->rows, labyrinth->cols = 0;
+            }   
+        }
+        // printf("%lld\n", n);
+    }
+    --labyrinth->rows;
+    // printf("%d %d\n", labyrinth->rows, labyrinth->cols);
+    // for(int i = 0; i < labyrinth->rows; ++i){
+        // printf("%s", labyrinth->map[i]);
+    // }
+    if(n < 0) {
+        perror("Load map failed.");
+        return false;
+    }
+    return true;
 }
-
+UnitTest(testLoadMap) {
+    Labyrinth labyrinth;
+    labyrinth.cols = labyrinth.rows = 0;
+    printf("testing.");
+    assert(loadMap(&labyrinth, "./maps/map.txt") == true);
+}
 Position findPlayer(Labyrinth *labyrinth, char playerId) {
     // TODO: Implement this function
     Position pos = {-1, -1};
--- labyrinth/labyrinth.h
+++ labyrinth/labyrinth.h
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #define MAX_ROWS 100
 #define MAX_COLS 100
 #define VERSION_INFO "Labyrinth Game"
