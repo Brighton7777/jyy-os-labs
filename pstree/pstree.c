85eab85db15da4fc7861e469b131b841fc7d4406
Brighton <1205704350@qq.com>
2026-02-05 22:53:39 +0800

    feat: implement printVersionInfo and parseOptions

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <string.h>
+#include <unistd.h>
+#include "pstree.h"
 
 int main(int argc, char *argv[]) {
+    
+    Options options = {0};
+    parseOptions(&options, argc, argv);
 
     return 0;
+}
+
+void printVersionInfo() {
+    printf("%s\n", VERSION_INFO);
+}
+
+void parseOptions(Options *options, const int argc, const char* const argv[]) {
+    for(int i = 1; i < argc; ++i) {
+        if(strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0) {
+            options->show_pid = true;
+        }
+        else if(strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--numeric-sort") == 0) {
+            options->sort_children_by_pid_asc = true;
+        }
+        else if(strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) {
+            // printf("--version\n");
+            if(argc != 2) {
+                perror("Error: --V or --version cannot be combined with other options.\n");
+                exit(1);
+            }
+            printVersionInfo();
+            exit(0);
+        }
+    }
 }
--- pstree/pstree.h
+++ pstree/pstree.h
@@ -3,7 +3,7 @@
 #define MAX_CHILD_NUMBER 50
 #define VERSION_INFO "pstree by zrq"
 
-struct Proc {
+typedef struct Proc {
     char *name;
     int pid;
     int ppid;
@@ -14,16 +14,18 @@ struct Proc {
     int children[MAX_CHILD_NUMBER];
     int child_cnt;
     int parent_idx;
-};
-struct Options {
+} Proc;
+
+typedef struct Options {
     bool show_pid;
     // bool show_ppid;
     // bool show_argv;
     // bool show_path;
     bool sort_children_by_pid_asc;
-};
+} Options;
 
+void printVersionInfo();
 int initProc(int pid);
-void initOptions(Options *options, const char* const argv[]);
+void parseOptions(Options *options, const int argc, const char* const argv[]);
 int getChildren(int ppid);
 void showTree(Options options);
